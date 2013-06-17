896926a269a16498d47acaf6209fc0948702f79f
Caio SBA <caiosba@gmail.com>
2013-06-17 00:39:52 -0300

    Ticket #1414 (Prototype): Appending user mentions to translated tweet

--- app/assets/javascripts/translatedesk/controllers/translation.js
+++ app/assets/javascripts/translatedesk/controllers/translation.js
@@ -4,13 +4,21 @@ angular.module('translatedesk.controllers').controller('TranslationController', 
   $scope.workbench = Tweet.workbench;
   
   $scope.prepareTranslation = function(t) {
-    var hashtags = [];
-    if (t && t.entities && t.entities.hashtags) {
-      for (var i=0; i < t.entities.hashtags.length; i++) {
-        hashtags.push('#' + t.entities.hashtags[i].text);
+    var hashtags = [],
+        mentions = [];
+    if (t && t.entities) {
+      if (t.entities.hashtags) {
+        for (var i=0; i < t.entities.hashtags.length; i++) {
+          hashtags.push('#' + t.entities.hashtags[i].text);
+        }
+      }
+      if (t.entities.user_mentions) {
+        for (var i=0; i < t.entities.user_mentions.length; i++) {
+          mentions.push('@' + t.entities.user_mentions[i].screen_name);
+        }
       }
     }
-    $scope.translatedTweet = (t ? 'TT @' + t.user.screen_name + ' ' + hashtags.join(' ') : '');
+    $scope.translatedTweet = (t ? 'TT @' + t.user.screen_name + ' ' + hashtags.concat(mentions).join(' ') : '');
   };
 
 }]);
